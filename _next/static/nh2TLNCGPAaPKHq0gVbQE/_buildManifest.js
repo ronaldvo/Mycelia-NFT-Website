167c3fa7b3c0bb4bbe12c20ae5710e30df63d0e2
Ron Vo <ronaldvo@gmail.com>
2021-10-19 21:06:24 -0700

    Updates

---
+++ _next/static/nh2TLNCGPAaPKHq0gVbQE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-ce4c9888be1cee875506.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
